ec2d4e531c0fa8c190c6a15d824ab53bb5a63af4
Nuno Valadão <nuno.valadao@tonicapp.com>
2024-02-17 15:17:22 +0000

    new changes in mariadb container

--- srcs/requirements/mariadb/tools/entrypoint.sh
+++ srcs/requirements/mariadb/tools/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-set -e
 
 # Create the /run/mysqld directory and change its ownership to mysql
 mkdir -p /run/mysqld
@@ -12,19 +11,6 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     # Start the server in the background
     mysqld_safe --datadir=/var/lib/mysql --user=mysql &
 
-    # Allow any signal which would kill a process to stop mysqld
-    for i in `seq 30 -1 0`; do
-        if echo 'SELECT 1' | mysql &> /dev/null; then
-            break
-        fi
-        sleep 1
-    done
-
-    if [ "$i" = 0 ]; then
-        echo >&2 'MySQL init process failed.'
-        exit 1
-    fi
-
     # Setup user accounts and database
     mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
     mysql -u root -e "CREATE USER IF NOT EXISTS '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
@@ -32,11 +18,6 @@ if [ ! -d "/var/lib/mysql/mysql" ]; then
     mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';"
     mysql -u root -e "FLUSH PRIVILEGES;"
 
-    # Stop the server
-    if ! kill -s TERM "$!" || ! wait "$!"; then
-        echo >&2 'MySQL init process failed.'
-        exit 1
-    fi
 fi
 
 # Start the server
